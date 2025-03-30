c23129723680efd8650e3b31c778b5a62750c00a
numbpill3d <nolove@incel.email>
2025-03-29 21:57:54 -0400

    more

--- next.config.js
+++ next.config.js
@@ -4,11 +4,10 @@ const { withSentryConfig } = require('@sentry/nextjs');
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-};
-
-const sentryWebpackPluginOptions = {
-  // Additional options for Sentry Webpack plugin
-  silent: true, // Suppresses all logs
+  env: {
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'https://basednet.lol',
+  },
 };
 
 // Configure Sentry options
@@ -20,4 +19,9 @@ const sentryOptions = {
   disableLogger: true,
 };
 
+const sentryWebpackPluginOptions = {
+  silent: true, // Suppresses all logs
+};
+
+// Exporting the combined configuration
 module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions, sentryOptions);
